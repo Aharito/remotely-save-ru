52ac1af34017d3756b3eea5a639fe4322acea7fb
Aharito <aharito@yandex.ru>
2025-08-30 23:13:29 +0700

    Ru translation

--- src/langs/index.ts
+++ src/langs/index.ts
@@ -1,9 +1,11 @@
 import en from "./en.json";
 import zh_cn from "./zh_cn.json";
 import zh_tw from "./zh_tw.json";
+import ru from "./ru.json";
 
 export const LANGS = {
   en: en,
   zh_cn: zh_cn,
   zh_tw: zh_tw,
+  ru: ru,
 };
